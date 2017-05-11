c249e1547a7ad425ff2744f36207600631a7eea2
Jeferson Camargo <Correo>
2017-05-11 15:04:16 -0500

    Se agrega modal que mustra el video Intro

--- Formularios/IntroJuego.php
+++ Formularios/IntroJuego.php
@@ -7,10 +7,13 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <script src="../Jquery/jquery-3.2.1.min.js" type="text/javascript"></script>
         <link rel="stylesheet" href="../Style/bootstrap/css/bootstrap.css">
         <title>Intro</title>
         <script>
-
+            function CerrarModal() {
+                $('#myModal').hide();
+            }
         </script>
         <style>
             body {
@@ -22,16 +25,90 @@ and open the template in the editor.
                 background-attachment: fixed;
             }
 
+            #myModal{
+                position: fixed;
+                height:100vh;
+                width:100vw;
+                z-index: 1;
+                top:0;
+                left: 0;
+                background-color: rgba(0, 0, 0, 0.5);
+            }
+            #contenidomodal{
+                position: relative;
+                background-color: #fff;
+                background-clip: padding-box;
+                border: 1px solid #999;
+                border: 1px solid rgba(0,0,0,.2);
+                border-radius: 10px;
+                outline: 0;
+                box-shadow: 0 3px 9px rgba(0,0,0,.5);
+                width: 95%;
+                height: 95%;
+                margin: auto;
+                margin-top: 1%;
+            }
+
+            #cabecera{
+                height: 35px;
+                background-color: black;
+                color: white;
+                font-size: 18px;
+                text-align: center;
+                font-family: Helvetica;
+                font-weight: bold;
+                padding: 5px;
+                border-bottom: 1px solid #e5e5e5;
+            }
+
+            .cntCabecera{
+                height: 40px;
+                display:inline-block;
+                vertical-align: top;
+                padding-top: 3px;
+            }
+
+            .btnCerrar{
+                width: 40px;
+                display:inline-block;
+                vertical-align: top;
+                float:right;
+                cursor:pointer;
+            }
+
+
+            body            {              
+                margin: 0;
+            }
+            .video-container{
+                top: 0%;
+                left: 0%;
+                height: 889px;
+                width: 100%;
+                overflow: hidden;
+            }
+            video.fillWidth {
+                width: 100%;
+            }
         </style>
     </head>
     <body> 
-        <a href="#modalVideo" class="btn btn-primary btn-lg" data-toggle="modal">Video</a>
-        <div class="modal fade" id="modalVideo">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <p class="modal-title">Prueba!</p>s
+        <div id="myModal"  >
+            <div id="contenidomodal">
+                <div id="cabecera">
+                    <div class="cntCabecera"></div>
+                    <div class="btnCerrar" onclick="CerrarModal()">
+                        <span>×</span>    
                     </div>
+                </div>
+                <div id="cuerpo">
+                    <div class="video-container">
+                        <video autoplay loop class="fillWidth visible-lg">
+                            <source src="../Video/IntroVideoJuego.mp4" type="video/mp4; "/>        
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+
                 </div>
             </div>
         </div>
